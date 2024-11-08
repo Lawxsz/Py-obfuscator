54189eaca3652e1b69b6aa0daeba9ef196c76d97
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2024-11-08 10:00:24 -0300

    Update obf.py

--- obf.py
+++ obf.py
@@ -1,4 +1,4 @@
-import random, string, base64, codecs, argparse, os, sys, subprocess, requests
+import random, string, base64, codecs, argparse, os, sys, subprocess
 from textwrap import wrap
 from lzma import compress
 from marshal import dumps
@@ -6,129 +6,113 @@ from marshal import dumps
 def printerr(data):
     print(data, file=sys.stderr)
 
-class prysmaxobf:
+class PrysmaticObfuscator:
     def __init__(self, code, outputpath):
         self.code = code.encode()
         self.outpath = outputpath
-        self.varlen = 3
+        self.varlen = random.randint(5, 10)
         self.vars = {}
 
         self.marshal()
         self.encrypt1()
         self.encrypt2()
-        self.encrypt3()  # Agrega encrypt3 para evitar problemas de detección
+        self.encrypt3()
         self.finalize()
 
     def generate(self, name):
         res = self.vars.get(name)
         if res is None:
-            res = "_" + "".join(["_" for _ in range(self.varlen)])
+            res = "_" + "".join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=self.varlen))
             self.varlen += 1
             self.vars[name] = res
         return res
 
-    def encryptstring(self, string, config={}, func=False):
-        b64 = list(b"base64")
-        b64decode = list(b"b64decode")
-        __import__ = config.get("__import__", "__import__")
-        getattr = config.get("getattr", "getattr")
-        bytes = config.get("bytes", "bytes")
-        eval = config.get("eval", "eval")
-        if not func:
-            return f'{getattr}({__import__}({bytes}({b64}).decode()), {bytes}({b64decode}).decode())({bytes}({list(base64.b64encode(string.encode()))})).decode()'
-        else:
-            attrs = string.split(".")
-            base = self.encryptstring(attrs[0], config)
-            attrs = list(map(lambda x: self.encryptstring(x, config, False), attrs[1:]))
-            newattr = ""
-            for i, val in enumerate(attrs):
-                if i == 0:
-                    newattr = f'{getattr}({eval}({base}), {val})'
-                else:
-                    newattr = f'{getattr}({newattr}, {val})'
-            return newattr
+    def encrypt_string(self, string):
+        b64 = base64.b64encode(string.encode()).decode()
+        return f'__import__("base64").b64decode("{b64}").decode()'
 
-    def encryptor(self, config):
-        def func_(string, func=False):
-            return self.encryptstring(string, config, func)
+    def encryptor(self):
+        def func_(string):
+            return self.encrypt_string(string)
         return func_
 
     def compress(self):
-        self.code = compress(self.code)
+        compressed_code = compress(self.code)
+        return base64.b64encode(compressed_code).decode()
 
     def marshal(self):
-        self.code = dumps(compile(self.code, "<string>", "exec"))
+        compiled_code = compile(self.code, "<string>", "exec")
+        self.code = dumps(compiled_code)
 
     def encrypt1(self):
-        code = base64.b64encode(self.code).decode()
-        partlen = int(len(code) / 4)
-        code = wrap(code, partlen)
-        var1 = self.generate("a")
-        var2 = self.generate("b")
-        var3 = self.generate("c")
-        var4 = self.generate("d")
-        init = [f'{var1}="{codecs.encode(code[0], "rot13")}"', f'{var2}="{code[1]}"', f'{var3}="{code[2][::-1]}"', f'{var4}="{code[3]}"']
+        code_b64 = base64.b64encode(self.code).decode()
+        parts = wrap(code_b64, random.randint(15, 40))
+        
+        var_names = [self.generate(f"var{i}") for i in range(4)]
+        
+        init_statements = [
+            f'{var_names[0]}="{codecs.encode(parts[0], "rot13")}"',
+            f'{var_names[1]}="{parts[1]}"',
+            f'{var_names[2]}="{parts[2][::-1]}"',
+            f'{var_names[3]}="{parts[3]}"'
+        ]
 
-        random.shuffle(init)
-        init = ";".join(init)
+        random.shuffle(init_statements)
+        
         self.code = f'''
-# Obfuscated https://t.me/lawxsz
-{init};__import__({self.encryptstring("builtins")}).exec(__import__({self.encryptstring("marshal")}).loads(__import__({self.encryptstring("base64")}).b64decode(__import__({self.encryptstring("codecs")}).decode({var1}, __import__({self.encryptstring("base64")}).b64decode("{base64.b64encode(b'rot13').decode()}").decode())+{var2}+{var3}[::-1]+{var4})))
+# Obfuscated Code by github.com/lawxsz/py-obfuscator
+{"; ".join(init_statements)};
+exec(__import__("marshal").loads(__import__("base64").b64decode(__import__("codecs").decode({var_names[0]}, "rot13") + {var_names[1]} + {var_names[2]}[::-1] + {var_names[3]})))
 '''.strip().encode()
 
     def encrypt2(self):
-        self.compress()
-        var1 = self.generate("e")
-        var2 = self.generate("f")
-        var3 = self.generate("g")
-        var4 = self.generate("h")
-        var5 = self.generate("i")
-        var6 = self.generate("j")
-        var7 = self.generate("k")
-        var8 = self.generate("l")
-        var9 = self.generate("m")
-
-        conf = {
-            "getattr": var4,
-            "eval": var3,
-            "__import__": var8,
-            "bytes": var9
-        }
-        encryptstring = self.encryptor(conf)
-
-        self.code = f'''# Obfuscated https://t.me/lawxsz
-{var3} = eval({self.encryptstring("eval")});{var4} = {var3}({self.encryptstring("getattr")});{var8} = {var3}({self.encryptstring("__import__")});{var9} = {var3}({self.encryptstring("bytes")});{var5} = lambda {var7}: {var3}({encryptstring("compile")})({var7}, {encryptstring("<string>")}, {encryptstring("exec")});{var1} = {self.code}
-{var2} = {encryptstring('__import__("builtins").list', func=True)}({var1})
+        compressed_code = self.compress()
+        
+        var_names = [self.generate(f"var{i}") for i in range(5)]
+        
+        self.code = f'''# Obfuscated Code
+{var_names[0]} = __import__("base64").b64decode("{compressed_code}");
 try:
-    {encryptstring('__import__("builtins").exec', func=True)}({var5}({encryptstring('__import__("lzma").decompress', func=True)}({var9}({var2})))) or {encryptstring('__import__("os")._exit', func=True)}(0)
-except {encryptstring('__import__("lzma").LZMAError', func=True)}:...
+    exec(__import__("marshal").loads({var_names[0]}))
+except Exception as e:
+    print("Decompression or execution error:", e)
 '''.strip().encode()
 
     def encrypt3(self):
-        self.compress()
-        data = base64.b64encode(self.code)
-        self.code = f'# Obfuscated https://t.me/lawxsz\n\nimport base64, lzma; exec(compile(lzma.decompress(base64.b64decode({data})), "<string>", "exec"))'.encode()
+        compressed_code = base64.b64encode(self.code).decode()
+        
+        # Adding an extra layer of obfuscation
+        obfuscated_exec = f'''
+import base64; 
+exec(base64.b64decode("{compressed_code}"))
+'''
+        
+        # Randomly shuffle the execution order to make it less predictable
+        shuffled_exec_parts = random.sample(obfuscated_exec.splitlines(), len(obfuscated_exec.splitlines()))
+        
+        self.code = '\n'.join(shuffled_exec_parts).encode()
 
     def finalize(self):
         if os.path.dirname(self.outpath).strip() != "":
             os.makedirs(os.path.dirname(self.outpath), exist_ok=True)
+        
         with open(self.outpath, "w", encoding='utf-8') as e:
             e.write(self.code.decode())
             print("Saved as --> " + os.path.realpath(self.outpath))
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(prog= sys.argv[0], description= "Obfuscates python program to make it harder to read")
-    parser.add_argument("FILE", help= "Path to the file containing the python code")
-    parser.add_argument("-o", type= str, help= 'Output file path [Default: "Obfuscated_<FILE>.py"]', dest= "path")
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Obfuscates Python program to make it harder to read")
+    parser.add_argument("FILE", help="Path to the file containing the Python code")
+    parser.add_argument("-o", type=str, help='Output file path [Default: "Obfuscated_<FILE>.py"]', dest="path")
+    
     args = parser.parse_args()
 
     if not os.path.isfile(sourcefile := args.FILE):
         printerr(f'No such file: "{args.FILE}"')
         os._exit(1)
     elif not sourcefile.endswith((".py", ".pyw")):
-        printerr('The file does not have a valid python script extention!')
+        printerr('The file does not have a valid Python script extension!')
         os._exit(1)
     
     if args.path is None:
@@ -137,24 +121,4 @@ if __name__ == "__main__":
     with open(sourcefile, encoding='utf-8') as sourcefile:
         code = sourcefile.read()
     
-    prysmaxobf(code, args.path)
-
-    parser = argparse.ArgumentParser(prog= sys.argv[0], description= "Obfuscates python program to make it harder to read")
-    parser.add_argument("FILE", help= "Path to the file containing the python code")
-    parser.add_argument("-o", type= str, help= 'Output file path [Default: "Obfuscated_<FILE>.py"]', dest= "path")
-    args = parser.parse_args()
-
-    if not os.path.isfile(sourcefile := args.FILE):
-        printerr(f'No such file: "{args.FILE}"')
-        os._exit(1)
-    elif not sourcefile.endswith((".py", ".pyw")):
-        printerr('The file does not have a valid python script extention!')
-        os._exit(1)
-    
-    if args.path is None:
-        args.path = "Obfuscated_" + os.path.basename(sourcefile)
-    
-    with open(sourcefile, encoding='utf-8') as sourcefile:
-        code = sourcefile.read()
-    
-    prysmaxobf(code, args.path)
+    PrysmaticObfuscator(code, args.path)
